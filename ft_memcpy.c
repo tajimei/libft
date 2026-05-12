0eeda00831c314a181fe4a6df724fb561dc88808
tajimei <g2560214@edu.cc.ocha.ac.jp>
2026-05-12 15:14:50 +0900

    5.12

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtajima <mtajima@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: meitajima <mtajima@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 20:57:38 by mtajima           #+#    #+#             */
-/*   Updated: 2026/04/29 13:14:09 by mtajima          ###   ########.fr       */
+/*   Updated: 2026/05/12 15:13:03 by meitajima        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*d;
 	const unsigned char	*s;
 
-	if (!dest && !src)
-		return (NULL);
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
 	while (n--)
--- ft_memmove.c
+++ ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtajima <mtajima@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: meitajima <mtajima@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 20:58:16 by mtajima           #+#    #+#             */
-/*   Updated: 2026/05/05 18:38:57 by mtajima          ###   ########.fr       */
+/*   Updated: 2026/05/12 15:13:19 by meitajima        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char		*d;
 	const unsigned char	*s;
 
-	if (!dest && !src)
-		return (NULL);
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
 	if (d > s)
